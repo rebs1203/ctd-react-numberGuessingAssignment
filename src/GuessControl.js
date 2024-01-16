a63e40972ca72d93fcdac47be6cb00f5150a3e58
rebecavg <rebecavgallo@hotmail.com>
2024-01-16 15:27:30 -0500

    Revert "Update instructions"
    This reverts commit 92a50479d03cf83e1b6fe0bbf55569df7f0a56f7.

--- src/GuessControl.js
+++ src/GuessControl.js
@@ -10,8 +10,16 @@ function GuessControl() {
     setCurrentGuess( Number(event.target.value) );
   }
 
+<<<<<<< HEAD
   const onSubmitGuess = () => {
     setCurrentGuess('')
+=======
+  onSubmitGuess() {
+    // Since the values from an HTML input are strings by default,
+    //  convert to a number for the returned guess value
+    this.props.onGuess(Number(this.state.currentGuess));
+    this.setState({ currentGuess: "" });
+>>>>>>> parent of 92a5047 (Update instructions)
   }
 
   return (
